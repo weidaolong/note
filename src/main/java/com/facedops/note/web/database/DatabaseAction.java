5397188fffbfb47816331ba3ac29822789ccbfcb
weidl <weidl@10.254.203.215>
2014-10-08 17:25:06 +0800

    A

--- src/main/java/com/facedops/note/web/database/DatabaseAction.java
+++ src/main/java/com/facedops/note/web/database/DatabaseAction.java
@@ -1,5 +1,7 @@
 package com.facedops.note.web.database;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,11 @@ import com.facedops.note.service.database.DatabaseService;
 @Controller
 @RequestMapping(value="/database")
 public class DatabaseAction {
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String show(){
-		service.backFile();
+	public String show(HttpServletRequest request){
+		String webPath=request.getSession().getServletContext().getRealPath("");
+		service.backFile(webPath);
 		return "/database/databaseShow";
 	}
 	@Autowired
