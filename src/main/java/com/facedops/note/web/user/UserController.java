66bf4ad5c7fc65a35fba40e882f37e01ccdb7822
weidl <weidl@10.254.203.215>
2014-10-08 17:47:26 +0800

    bb

--- src/main/java/com/facedops/note/web/user/UserController.java
+++ src/main/java/com/facedops/note/web/user/UserController.java
@@ -1,5 +1,8 @@
 package com.facedops.note.web.user;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -17,54 +20,76 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.facedops.note.entity.rbac.Users;
 import com.facedops.note.service.user.UserService;
+import com.facedops.note.util.hibernate.ConfigProperties;
 import com.facedops.note.web.utils.Page;
 
 @Controller
-@RequestMapping(value="/user")
+@RequestMapping(value = "/user")
 public class UserController {
 	@Autowired
 	private UserService userService;
-	
-	private static Logger logger = LoggerFactory.getLogger(UserController.class);
-	
+
+	private static Logger logger = LoggerFactory
+			.getLogger(UserController.class);
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String list(Page page,HttpServletRequest request,Model model,Users users) {
-		
-		org.springframework.data.domain.Page<Users> userList=userService.getUserList(page,users);
+	public String list(Page page, HttpServletRequest request, Model model,
+			Users users) {
+
+		org.springframework.data.domain.Page<Users> userList = userService
+				.getUserList(page, users);
 		model.addAttribute("userList", userList);
 		model.addAttribute("users", users);
 		model.addAttribute("page", page);
 		model.addAttribute("searchParams", request.getQueryString());
 		return "user/userList";
 	}
-	
+
 	@RequestMapping(value = "create", method = RequestMethod.GET)
 	public String createForm() {
 		return "user/userForm";
 	}
-	
+
 	@RequestMapping(value = "create", method = RequestMethod.POST)
-	public String create(@RequestParam(value = "file", required = false) MultipartFile file,@ModelAttribute("users") Users users) {
-		logger.info(file.getName());
+	public String create(
+			@RequestParam(value = "file", required = false) MultipartFile file,
+			HttpServletRequest request, @ModelAttribute("users") Users users) {
+		logger.info(file.getOriginalFilename());
+		String path = request.getSession().getServletContext().getRealPath("");
+		String fileName = file.getOriginalFilename();
+
+		File targetFile = new File(path+ConfigProperties.getConfig("FILE_PATH"), fileName);
+		if (!targetFile.exists()) {
+			targetFile.mkdirs();
+		}
+		try {
+			file.transferTo(targetFile);
+		}  catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
 		userService.save(users);
 		return "redirect:/user";
 	}
-	
+
 	@RequestMapping(value = "delete/{id}")
-	public String delete(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+	public String delete(@PathVariable("id") Long id,
+			RedirectAttributes redirectAttributes) {
 		redirectAttributes.addFlashAttribute("message", "删除任务成功");
 		userService.delete(id);
 		return "redirect:/user";
 	}
-	
+
 	@RequestMapping(value = "update/{id}", method = RequestMethod.GET)
 	public String updateForm(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("user", userService.getUsers(id));
 		return "/user/userForm";
 	}
-	
+
 	@ModelAttribute
-	public void getUser(@RequestParam(value = "id", defaultValue = "-1") Long id, Model model) {
+	public void getUser(
+			@RequestParam(value = "id", defaultValue = "-1") Long id,
+			Model model) {
 		if (id != -1) {
 			model.addAttribute("task", userService.getUsers(id));
 		}
