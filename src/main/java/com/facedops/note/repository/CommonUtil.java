321555f4e918f5a985004dc1abf22ab1a5ad70dd
weidl <weidl@10.254.203.215>
2014-10-09 12:23:34 +0800

    aa

--- src/main/java/com/facedops/note/repository/CommonUtil.java
+++ src/main/java/com/facedops/note/repository/CommonUtil.java
@@ -1,20 +1,21 @@
 package com.facedops.note.repository;
 
+import javax.persistence.EntityManager;
+
 import org.hibernate.Session;
-import org.hibernate.jpa.HibernateEntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CommonDao {
-	private final HibernateEntityManager em;
+public class CommonUtil {
+	private EntityManager em;
 
 	public Session getSession() {
-		return em.getSession();
+		return em.unwrap(Session.class);
 	}
 
 	@Autowired
-	public CommonDao(HibernateEntityManager em) {
+	public CommonUtil(EntityManager em) {
 		this.em = em;
 	}
 }
